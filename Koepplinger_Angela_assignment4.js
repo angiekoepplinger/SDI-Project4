2be80634232ba5221fd351ef11a5dd5c0e404da1
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-21 21:06:10 -0500

    checkEmail & checkPercent
    finished checkEmail function, started and completed checkPercent
    function.

--- Koepplinger_Angela_assignment4.js
+++ Koepplinger_Angela_assignment4.js
@@ -1,5 +1,6 @@
 
 var stringLibrary = function () {
+ 	
 	var checkPhone = function (string) {
 		
 		//To get 123 digits
@@ -16,55 +17,92 @@ var stringLibrary = function () {
 		var end = string.length;						//define ending position
 		var str3 = string.substring(start, end);
 
-		console.log(str1 + " " + str2 + " " + str3);
-
 		if (!isNaN(str1) && !isNaN(str2) && !isNaN(str3)) {  //if str1,2,3 is not Not a Number(is a number)=true
+			if(str1.length === 3 && str2.length === 3 && str3.length === 4){
+				return true;
+			} else {
+				return false;
+			};
+		} else {
+			return false;
+		};
+	};
+
+	var titleCase = function (string) {
+		var words = string.split(" ");							//splits the phrase into an array called "words"	
+		var combine = "";										//creates a variable to hold the concatinated capitalized words
+		for (var i=0; i < words.length; i++) {					
+			var upperCase = words[i].replace(words[i].charAt(0),words[i].charAt(0).toUpperCase()); 
+ 			combine = combine.concat(upperCase + " ");
+		};
+		return combine;
+	};
+
+	var checkEmail = function (email) {
+		var atStart = email.indexOf("@");
+		var periodStart = email.lastIndexOf(".");
+		if ((atStart > 0) && (periodStart > 2) && (atStart < periodStart) && ((email.length - 1) - periodStart > 1) && 
+			((email.length - 1) - periodStart < 4) && (atStart === (email.lastIndexOf("@")))) {
 			return true;
 		} else {
 			return false;
 		};
-	}; 
+	};	
+
+	var checkPercent = function (number1, number2, range) {
+		var lessThan = number2 -((range/100)*number2);
+		var moreThan = number2 + ((range/100)*number2);
+		if (number1 < lessThan || number1 > moreThan) {
+			return false;
+		} else {
+			return true;
+		};
+	};
+	
+
+
 	return {
-		"checkPhone" : checkPhone
+		"checkPhone"   : checkPhone,
+		"titleCase"    : titleCase,
+		"checkEmail"   : checkEmail,
+		"checkPercent" : checkPercent
 	};
-};
+};		
+	
 
-var phoneNumber = "123-456-7890";
-var checkStrings = stringLibrary();
-var phoneCheckResult = checkStrings.checkPhone(phoneNumber);
-console.log(phoneCheckResult);
-
-
-
-// //---------------
-
-// 	var checkEmail = function (string) {
-// 	if ()
-// }
-
-
-
-//----------------
-
-var titleCase = function (string) {
-	var words = string.split(" ");							//splits the phrase into an array
-	console.log(words);	
-	var result = "";									//console logs the array
-	for (var i=0; i < words.length; i++) {					//loop through the array of words
-		/*var firstLetter = words[i].charAt(0);				//identify first letter of each word
-		var capital = firstLetter.toUpperCase();			//capitalize first letter at words[i], character 0
-		var upperCase = words[i].replace(firstLetter,capital);	//replace firstletter with capitalized version
-		*/
-		var upperCase = words[i].replace(words[i].charAt(0),words[i].charAt(0).toUpperCase()); 
-		console.log(upperCase);
- 		result = result.concat(upperCase + " ");
-	};
-	return result;
-};
-
-
-var phrase = "The cow jumped over the moon"
-console.log(titleCase(phrase));
+// //----------------
 
 
 //checkPhone Call
+var phoneNumber = "123-456-7890";
+var checkStrings = stringLibrary();
+var phoneCheckResult = checkStrings.checkPhone(phoneNumber);
+console.log("phoneCheck Result: " + phoneCheckResult);
+
+// //titleCase Call
+var phrase = "The cow jumped over the moon"
+console.log("titleCase Result: " + checkStrings.titleCase(phrase));
+
+//checkEmail Call
+var emailAddress = "a@b.co@";
+var checkStrings = stringLibrary();
+var emailCheckResult = checkStrings.checkEmail(emailAddress);
+console.log("emailCheck Result: " + emailCheckResult);
+
+//checkPercent Call
+var no1 = 89
+var no2 = 100
+var percent = 10
+var checkPercent = stringLibrary();
+var checkPercentResult = checkStrings.checkPercent(no1, no2, percent);
+console.log("checkPercent Result: " + checkPercentResult);
+
+
+
+
+
+
+
+
+
+
