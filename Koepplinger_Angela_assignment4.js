75d60570299d06a7afa9bfe3d173a723ae8b9d3e
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-08-22 21:34:53 -0500

    compareDates
    method to figure out the Days or Hours difference between tomorrow and
    my birthday.

--- Koepplinger_Angela_assignment4.js
+++ Koepplinger_Angela_assignment4.js
@@ -1,22 +1,15 @@
-
+//String Class
 var stringLibrary = function () {
  	
 	var checkPhone = function (string) {
-		
-		//To get 123 digits
-		var end = string.indexOf("-");  				//define ending position, starts at 0
-		var str1 = string.substring(0,end);							
-		
-		//To get 456 digits
-		var start = string.indexOf("-")+1;				//define starting position
-		var end = string.lastIndexOf("-");				//define ending position
-		var str2 = string.substring(start,end)
-
-		//To get last 4 digits, 7890:
-		var start = string.lastIndexOf("-")+1;  		//define starting position
-		var end = string.length;						//define ending position
-		var str3 = string.substring(start, end);
-
+		var end1 = string.indexOf("-");  				//define ending position, starts at 0
+		var str1 = string.substring(0,end1);							
+		var start2 = string.indexOf("-")+1;				//define starting position
+		var end2 = string.lastIndexOf("-");				//define ending position
+		var str2 = string.substring(start2,end2)
+		var start3 = string.lastIndexOf("-")+1;  		//define starting position
+		var end3 = string.length;						//define ending position
+		var str3 = string.substring(start3, end3);
 		console.log(str1 + " " + str2 + " " + str3);
 
 		if (!isNaN(str1) && !isNaN(str2) && !isNaN(str3)) {  //if str1,2,3 is not Not a Number(is a number)=true
@@ -51,16 +44,23 @@ var stringLibrary = function () {
 		};
 	};	
 
+	var separatorChange = function (str1, newChar) {
+ 		newstr = str1.replace(/\W/g,newChar);
+ 		return newstr
+ 	};
+
 	return {
-		"checkPhone"   : checkPhone,
-		"titleCase"    : titleCase,
-		"checkEmail"   : checkEmail,
+		"checkPhone"      : checkPhone,
+		"checkEmail"      : checkEmail,
+		"titleCase"       : titleCase,
+		"separatorChange" : separatorChange
 	};
 };		
 
+//Math Class
 var mathLibrary = function () {
 
-	var checkPercent = function (number1, number2, range) {
+	var fuzzyMatch = function (number1, number2, range) {
 		if ((number1 < number2 - ((range/100)*number2)) || (number1 > number2 + ((range/100)*number2))) {
 			return false;
 		} else {
@@ -75,62 +75,73 @@ var mathLibrary = function () {
  	
  	var stringToNum = function (string) {
  		var number = parseFloat("42.2")
- 		return number
+ 		return number;
+ 	};
 
-
+ 	var compareDates =  function (d1, d2, units) {
+ 		var output =[];
+ 		var hrs  = 1000*60*60;
+ 		var days = 1000*60*60*24;
+ 		output[0] = (((d2.getTime() - d1.getTime())/days));
+ 		output[1] = (((d2.getTime() - d1.getTime())/hrs));
+ 		
+ 		return output;
  	};
 
  	return {
-		"checkPercent" : checkPercent,
-		"formatNumber" : formatNumber,
-		"stringToNum"  : stringToNum
+		"formatDecimal" : formatNumber,
+		"fuzzyMatch"    : fuzzyMatch,
+		"stringToNum"   : stringToNum,
+		"compareDates"  : compareDates
 	};
-
 
 };
 
-	
-
-
-	
-	
-
-// //----------------
-
-
-//checkPhone Call
-var phoneNumber = "123-456-7890";
+//Global Variables
 var checkStrings = stringLibrary();
+var checkMath = mathLibrary();	
+
+//STRING CALLS::
+//checkPhone
+var phoneNumber = "123-456-7890";
 var phoneCheckResult = checkStrings.checkPhone(phoneNumber);
 console.log("phoneCheck Result: " + phoneCheckResult);
 
-// //titleCase Call
-var phrase = "The cow jumped over the moon"
-console.log("titleCase Result: " + checkStrings.titleCase(phrase));
-
-//checkEmail Call
+//checkEmail
 var emailAddress = "a@k.co@";
-var checkStrings = stringLibrary();
 var emailCheckResult = checkStrings.checkEmail(emailAddress);
 console.log("emailCheck Result: " + emailCheckResult);
 
-//checkPercent Call
+//titleCase Call
+var phrase = "The cow jumped over the moon"
+console.log("titleCase Result: " + checkStrings.titleCase(phrase));
+
+//separatorCheck 
+var string1 = "a,b,c";
+var separatorChangeResult = checkStrings.separatorChange(string1,"/");
+console.log(separatorChangeResult);
+
+
+// NUMBER CALLS::
+//formatDecimal
+var money = 5.827
+var formatDecimalResult = checkMath.formatDecimal(5.8);
+console.log(formatDecimalResult);
+
+//fuzzyMatch Call
 var no1 = 90
 var no2 = 100
 var percent = 10
-var checkMath = mathLibrary();
-var checkPercentResult = checkMath.checkPercent(no1, no2, percent);
-console.log("checkPercent Result: " + checkPercentResult);
+var fuzzyMatchResult = checkMath.fuzzyMatch(no1, no2, percent);
+console.log("fuzzyMatch Result: " + fuzzyMatchResult);
 
-//formatNumber
-var money = 5.827
-var checkMath = mathLibrary();
-var formatNumberResult = checkMath.formatNumber(money);
-console.log(formatNumberResult);
+//compareDates
+var date1 = new Date(2012,8,23);
+var date2 = new Date(2013,2,19);
+var compareDatesResult = checkMath.compareDates(date1, date2);
+console.log(compareDatesResult[0] + " days, or " + compareDatesResult[1] + " hrs");
 
 //stringToNumber
-var stringNum= "42"
-var checkMath = mathLibrary();
 var stringToNumResult = checkMath.stringToNum("42");
 console.log(stringToNumResult);
 
